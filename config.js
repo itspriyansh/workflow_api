1debc04c210acbb241246f6a525f87f99aa1215b
Ayushi <ayushisamdani98.as@gmail.com>
2019-09-21 10:28:19 +0530

    Route added for user details

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
     'secretKey': '98284-90944-75055-82095',
-    'mongoUrl': 'mongodb://localhost:27017/projectManagement'
+    'mongoUrl': 'mongodb+srv://ayushi:samdani98@cluster0-qg9ej.mongodb.net/test?retryWrites=true&w=majority'
 }
