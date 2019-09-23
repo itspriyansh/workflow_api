a96135beae4eb429972bc3b8e456a9d97284dc62
Ayushi <ayushisamdani98.as@gmail.com>
2019-09-23 14:00:18 +0530

    Removed Consoles

--- routes/resolvePath.js
+++ routes/resolvePath.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var auth = require('../authenticate');
-var filemanagerMiddleware = require('@opuscapita/filemanager-server').middleware;
 var fs = require('fs');
 
 var router = express.Router();
@@ -8,7 +7,6 @@ var router = express.Router();
 router.post('/', auth.verifyUser, auth.verifyAction('upload'), (req, res) => {
   let pathArr = req.body.path.split('/');
   let pathStr = __dirname.slice(0,-7);
-  console.log(pathStr);
   pathArr.forEach(p => {
     pathStr = pathStr+'/'+p;
     if(!fs.existsSync(pathStr)){
