338d93b25240cbc340c84beaea1883e3a3dd2961
Priyansh Bhardwaj <priyanshbhj@gmail.com>
2019-10-25 17:17:14 +0530

    Integrated with client side

---
+++ public/precache-manifest.b5d07dee08cd4bc8b4bf9ebfc809ce9d.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "186f130accdfcd9432699dd90699ee20",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e2b5748c688c70226602",
+    "url": "/static/css/2.133b1acd.chunk.css"
+  },
+  {
+    "revision": "0e230c7fee0f4aecae82",
+    "url": "/static/css/main.262c85f4.chunk.css"
+  },
+  {
+    "revision": "e2b5748c688c70226602",
+    "url": "/static/js/2.2fe5f6d7.chunk.js"
+  },
+  {
+    "revision": "0e230c7fee0f4aecae82",
+    "url": "/static/js/main.470dd618.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "b7cf2d4b6c827b33daa5a64c9b413672",
+    "url": "/static/media/addp.b7cf2d4b.png"
+  },
+  {
+    "revision": "500ad8bd92fb9782106199dc63534b64",
+    "url": "/static/media/folfol.500ad8bd.png"
+  },
+  {
+    "revision": "086d36f7ec59f30dae0232709a0597ed",
+    "url": "/static/media/user_access.086d36f7.png"
+  }
+]);
