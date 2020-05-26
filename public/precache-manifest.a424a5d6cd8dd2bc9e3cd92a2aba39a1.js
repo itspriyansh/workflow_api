91d92ebb4d2a7e7f27fbc7538868ce2dd919d7b6
Ajay Prajapat <ajayprajapat@live.com>
2020-05-26 17:04:19 +0530

    freezed

---
+++ public/precache-manifest.a424a5d6cd8dd2bc9e3cd92a2aba39a1.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0db5a302af2335c53cf3ea0847602ef4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0c961e28836f3dd5fd92",
+    "url": "/static/css/2.da3d095b.chunk.css"
+  },
+  {
+    "revision": "05fc5b54c42f94c7fad5",
+    "url": "/static/css/main.6637156e.chunk.css"
+  },
+  {
+    "revision": "0c961e28836f3dd5fd92",
+    "url": "/static/js/2.af6de503.chunk.js"
+  },
+  {
+    "revision": "05fc5b54c42f94c7fad5",
+    "url": "/static/js/main.f161e7ca.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "b7cf2d4b6c827b33daa5a64c9b413672",
+    "url": "/static/media/addp.b7cf2d4b.png"
+  },
+  {
+    "revision": "500ad8bd92fb9782106199dc63534b64",
+    "url": "/static/media/folfol.500ad8bd.png"
+  },
+  {
+    "revision": "086d36f7ec59f30dae0232709a0597ed",
+    "url": "/static/media/user_access.086d36f7.png"
+  }
+]);
---
+++ public/precache-manifest.d9d72c51a494ee97b92969ee87de4aad.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb0aceade9deccc9f033ad5c0ef7cc4a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4d289e460a48b97a8ff1",
+    "url": "/static/css/2.da3d095b.chunk.css"
+  },
+  {
+    "revision": "7bb45c3b25ea0b1b4fc2",
+    "url": "/static/css/main.7580772d.chunk.css"
+  },
+  {
+    "revision": "4d289e460a48b97a8ff1",
+    "url": "/static/js/2.a858d6a7.chunk.js"
+  },
+  {
+    "revision": "7bb45c3b25ea0b1b4fc2",
+    "url": "/static/js/main.df683772.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "b7cf2d4b6c827b33daa5a64c9b413672",
+    "url": "/static/media/addp.b7cf2d4b.png"
+  },
+  {
+    "revision": "500ad8bd92fb9782106199dc63534b64",
+    "url": "/static/media/folfol.500ad8bd.png"
+  },
+  {
+    "revision": "086d36f7ec59f30dae0232709a0597ed",
+    "url": "/static/media/user_access.086d36f7.png"
+  }
+]);
